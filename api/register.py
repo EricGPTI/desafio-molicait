866510e12791e34343d9ec3f9b772e55447788d1
Eric <eric.silva@nsprevention.com.br>
2020-04-10 00:48:50 -0300

    Criado função de update.

--- api/register.py
+++ api/register.py
@@ -31,13 +31,40 @@ def save_pagamento(request):
     return p
 
 
-def get_pagamento():
-    pagamento = Pagamento.get_pagamento(db)
-    return pagamento
+def get_pagamentos():
+    pagamentos = Pagamento.get_pagamentos(db)
+    return pagamentos
 
 
-def update_pagamento(session):
-    pass
+def update_pagamento(request):
+    if request.data.get('session') is not None:
+        session = request.data.get('session')
+        total = request.data.get('total')
+        pago = request.data.get('pago')
+
+        pagamento_salvo = Pagamento.get_pagamento(db, session)
+        total_salvo = pagamento_salvo.get('total')
+        pago_salvo = pagamento_salvo.get('pago')
+
+        if total is not None:
+            if pago is not None:
+                pgto = Pagamento(total, pago)
+                valores = pgto.pagamento()
+                cedulas = str(valores[0])
+                moedas = str(valores[1])
+                data = datetime.now().strftime('%d/%m/%Y')
+
+                u = {
+                    'session': str(session),
+                    'data': data,
+                    'total': str(total),
+                    'pago': str(pago),
+                    'troco': [cedulas, moedas]
+                }
+                update_result = Pagamento.update_pagamento(db, u)
+                registro_modificado = update_result.raw_result.get('nModified')
+                return registro_modificado
+    return None
 
 
 
