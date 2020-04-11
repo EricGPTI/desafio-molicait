6c0d64c12e9ffb2e20f1f442a7e31aaf0c772420
Eric <eric.silva@nsprevention.com.br>
2020-04-11 00:40:35 -0300

    Criada url 'home' para redirecionar para pagamentos.

--- molica/urls.py
+++ molica/urls.py
@@ -5,6 +5,7 @@ from api import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.pagamentos, name='pagamentos'),
     path('api/v1/pagamentos', views.pagamentos, name='pagamentos'),
     path('api/v1/pagamento/update/', views.update_pagamento, name='update'),
     path('api/v1/pagamento/delete/<str:id_pagamento>', views.delete_pagamento, name='delete'),
